0b28b045e1fb23ab3fbcd5132736c07e8f118798
HYEJI <lwiiiiii@naver.com>
2018-10-16 23:27:07 +0900

    알고리즘 문제 풀이

---
+++ baekjoon/IO/B2439.java
@@ -0,0 +1,26 @@
+package baekjoon.IO;
+
+import java.util.Scanner;
+
+public class B2439 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+
+		int n = in.nextInt();
+
+		StringBuilder builder = new StringBuilder();
+		for(int i = 1; i <= n; ++i) {
+			for(int j = 1; j <= n ; ++j) {
+				if(j <= n-i) {
+					builder.append(" ");
+				}
+				else
+					builder.append("*");
+			}
+			builder.append("\n");
+		}
+		System.out.println(builder.toString());
+	}
+
+}

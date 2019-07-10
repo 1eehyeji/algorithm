f861e93a5b4e22d0661c3872082b83b28edfb9f9
HYE_JI <lwiiiiii@naver.com>
2019-07-10 21:00:54 +0900

    11004번, 11729번 시간 초과 문제 해결

--- baekjoon/divideAndConquer/B11729.java
+++ baekjoon/divideAndConquer/B11729.java
@@ -1,25 +1,36 @@
 package baekjoon.divideAndConquer;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
+// 11729번 하노이 탑 이동 순서 (https://www.acmicpc.net/problem/11729)
+// 시간 초과 문제 BufferedWriter 로 해결
 public class B11729 {
 
-	static void solve(int n, int x, int y) {
+	static BufferedWriter bw;
+
+	static void solve(int n, int x, int y) throws Exception {
 		if(n == 0) return;
 		solve(n-1, x, 6-x-y);	// 제일 아래쪽 탑을 제외한 탑의 이동순서
-		System.out.println(x + " " + y);	// 아래쪽 탑을 3으로 이동함
+		bw.write(x + " " + y + "\n");	// 아래쪽 탑을 3으로 이동함
 		solve(n-1, 6-x-y, y);	// 나머지 탑을 3으로 옮기는 순서
 	}
 
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		int n = Integer.parseInt(br.readLine());
 
-		System.out.println((int) (Math.pow(2, n) - 1)); // 이동횟수는 d[n-1] + d[n-1] + 1 = 2 * d[n-1] + 1 = 2^n - 1
+		bw.write((int)(Math.pow(2, n) - 1) + "\n"); // 이동횟수는 d[n-1] + d[n-1] + 1 = 2 * d[n-1] + 1 = 2^n - 1
 
 		solve(n, 1, 3);
+
+		bw.flush();
+		bw.close();
+		br.close();
 	}
 
 }
--- baekjoon/sort/B11004.java
+++ baekjoon/sort/B11004.java
@@ -2,9 +2,10 @@ package baekjoon.sort;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.util.stream.Collectors;
 
+// 11004번 K번째 수 (https://www.acmicpc.net/problem/11004)
 public class B11004 {
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,16 +15,16 @@ public class B11004 {
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
 
-        int[] arr = new int[n];
+        List<Integer> list = new ArrayList();
 
         st = new StringTokenizer(br.readLine(), " ");
 
         for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+            list.add(Integer.parseInt(st.nextToken()));
         }
 
-        Arrays.sort(arr);
+        list = list.stream().sorted().collect(Collectors.toList());
 
-        System.out.println(arr[k - 1]);
+        System.out.println(list.get(k-1));
     }
 }

8c320b25ce77a011e2093dcd80d9d29774886206
HYE_JI <1eehyeji_@naver.com>
2020-08-05 22:50:12 +0900

    programmers level 1: 문자열 내림차순으로 배치하기
    문자열에 나타나는 문자를 큰것부터 작은 순으로 정렬

---
+++ programmers/SortStringInDescending.java
@@ -0,0 +1,19 @@
+package programmers;
+
+import java.util.*;
+
+public class SortStringInDescending {
+	static public String solution(String s) {
+		String answer = "";
+
+		String[] alphabets = s.split("");
+		Arrays.sort(alphabets, Collections.reverseOrder());
+
+		answer = String.join("", alphabets);
+
+		return answer;
+	}
+	public static void main(String[] args) {
+		System.out.println(solution("Zbcdefg"));
+	}
+}

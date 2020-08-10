a29aef373a97b05cd4b188eaa6e24f6a1fc2fbb5
HYE_JI <1eehyeji_@naver.com>
2020-08-10 17:36:35 +0900

    programmers level 1 : 문자열 다루기 기본
    문자열의 길이가 4 또는 6이고,
    문자열이 숫자로만 이루어져 있는지 확인하는 코드를 구현
    Integer.parseInt 메소드르 사용하여 문자열이 숫자로 이루어져 있는지
    확인함
    문자열에 숫자가 아닌 문자가 섞여있는 경우 Exception이 발생하고,
    try catch문을 사용하여 처리하는 방식을 구현
    문자열의 길이가 4 또는 6이 아닌 경우에도 Exception을 발생시켜 위와
    동일하게 처리함

---
+++ programmers/StringCheck.java
@@ -0,0 +1,28 @@
+package programmers;
+
+/* 문자열 다루기 기본
+*
+* 문자열을 int로 변환해주는 메소드를 사용하여 구현
+* 문자열에 숫자가 아닌 문자가 섞여있는 경우 NumberFormatException이 발생하고,
+* try catch문을 사용하여 Exception이 발생한 경우 false를 return 하도록 구현함
+* 문자열의 길이가 4 또는 6이 아닌 경우 Exception을 발생시켜 위와 동일하게 처리함
+*/
+public class StringCheck {
+	public static boolean solution(String s) {
+		boolean answer = true;
+
+		try {
+			if (s.length() != 4 && s.length() != 6)
+				throw new Exception();				// Exception을 발생시킴
+
+			int number = Integer.parseInt(s); 		// 문자열을 int형으로 변환하는 메소드
+		} catch (Exception e) {						// Exception이 발생하면 잡아서 처리함
+			answer = false;
+		}
+
+		return answer;
+	}
+
+	public static void main(String[] args) {
+	}
+}

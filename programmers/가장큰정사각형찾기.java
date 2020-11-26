4c1698d8be813b68ebfa2fe47a7b7401fd4d22e6
HYE_JI <leehj.kk@gmail.com>
2020-11-26 21:35:18 +0900

    programmers level 2 가장 큰 정사각형 찾기
    - DP를 사용해서 해결함
    - [1][1]에서부터 배열 대각선 위의 값과 바로 바로 위, 바로 왼쪽 칸 숫자
      중 가장 작은 값에 1을 더함
    - 가장 큰 값을 찾아서 제곱한 값을 반환
    - 행 또는 열의 길이가 1이면 가장 큰 정사각형의 크기는 1

---
+++ programmers/가장큰정사각형찾기.java
@@ -0,0 +1,31 @@
+package programmers;
+
+public class 가장큰정사각형찾기 {
+	static int solution(int[][] board) {
+		if (board.length == 1 || board[0].length == 1) {
+			return 1;
+		}
+
+		int maxValue = 0;
+		for (int i = 1; i < board.length; ++i) {
+			for (int j = 1; j < board[0].length; ++j) {
+				if (board[i][j] != 0) {
+					board[i][j] = Math.min(board[i - 1][j],
+							Math.min(board[i][j - 1], board[i - 1][j - 1])) + 1;
+					maxValue = Math.max(maxValue, board[i][j]);
+				}
+			}
+		}
+
+		return maxValue * maxValue;
+	}
+
+	public static void main(String[] args) {
+		int[][] board = {
+			{0, 0, 1, 1},
+			{1, 1, 1, 1}
+		};
+
+		System.out.println(solution(board));
+	}
+}

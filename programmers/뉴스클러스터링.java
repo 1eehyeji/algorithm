2c4bc00a7651e460f195c1b801b7519ccce49230
HYE_JI <leehj.kk@gmail.com>
2020-12-24 14:20:51 +0900

    programmers level 2 뉴스 클러스터링

--- programmers/뉴스클러스터링.java
+++ programmers/뉴스클러스터링.java
@@ -9,23 +9,23 @@ public class 뉴스클러스터링 {
 		Map<String, Integer> str1Tokens = getTokens(str1.toLowerCase());
 		Map<String, Integer> str2Tokens = getTokens(str2.toLowerCase());
 
-		double intersectionSize = 0;
-		double unionSize = 0;
+		float intersectionSize = 0;
+		float unionSize = 0;
 
 		for (String key : str1Tokens.keySet()) {
-			Integer count1 = str2Tokens.get(key);
-			Integer count2 = str1Tokens.get(key);
+			Integer tokenCount1 = str1Tokens.get(key);
+			Integer tokenCount2 = str2Tokens.get(key);
 
-			if (count1 == null) {
-				unionSize += count2;
+			if (tokenCount2 == null) {                               				// str1Tokens에만 존재하는 값을 합집합에 포함시킴
+				unionSize += tokenCount1;
 			} else {
-				intersectionSize += Math.min(count1, count2);
-				unionSize += Math.max(count1, count2);
-				str2Tokens.remove(key);
+				intersectionSize += Math.min(tokenCount1, tokenCount2);   			// 동일한 토큰을 가지면 교집합에 더 적은 토큰 개수를 더함
+				unionSize += Math.max(tokenCount1, tokenCount2);          			//            ``             더 많은 토큰 개수를 더함
+				str2Tokens.remove(key);                         					// 처리한 토큰은 삭제 (str1Tokens에는 없는 값만 남기기 위해)
 			}
 		}
 
-		for (String key : str2Tokens.keySet()) {
+		for (String key : str2Tokens.keySet()) {                					// str2Tokens에만 존재하는 값을 합집합에 포함시킴
 			unionSize += str2Tokens.get(key);
 		}
 
@@ -42,7 +42,7 @@ public class 뉴스클러스터링 {
 		for (int i = 0; i < (str.length() - 1); ++i) {
 			String token = str.substring(i, i + 2);
 
-			if (token.matches("[a-z]+")) {
+			if (token.matches("[a-z]+")) {                              		// 알파벳으로만 이루어진 토큰인지 확인
 				tokens.put(token, tokens.getOrDefault(token, 0) + 1);
 			}
 		}

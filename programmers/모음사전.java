7403ed0adf03436b09d34d84abfae5b2e7736027
1eehyeji <leehj.kk@gmail.com>
2021-09-16 22:09:16 +0900

    programmers 위클리 챌린지 5주차 모음사전, 7주차 입실퇴실

---
+++ programmers/모음사전.java
@@ -0,0 +1,45 @@
+package programmers;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class 모음사전 {
+
+    static String[] vowel = {"A", "E", "I", "O", "U"};
+
+    public static int solution(String word) {
+        int answer = 0;
+
+        Set<String> set = new TreeSet<>();
+
+        for (int i = 1; i <= 5; ++i) {
+            rePermutation(vowel.length, i, new StringBuilder(), set);
+        }
+
+        for (String s : set) {
+            ++answer;
+            if (s.equals(word)) break;
+        }
+
+        return answer;
+    }
+
+    private static void rePermutation(int n, int r, StringBuilder sb, Set<String> set) {
+        if (sb.length() == r) {
+            set.add(sb.toString());
+            return;
+        }
+
+        for (int i = 0; i < n; ++i) {
+            sb.append(vowel[i]);
+            rePermutation(n, r, sb, set);
+            sb.delete(sb.length() - 1, sb.length());
+        }
+    }
+
+    public static void main(String[] args) {
+        String word = "I";
+
+        System.out.println(solution(word));
+    }
+}

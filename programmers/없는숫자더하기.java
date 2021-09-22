e493b26ff9d5bc7c7b9de98c9319a47e134cf9da
1eehyeji <leehj.kk@gmail.com>
2021-09-22 22:40:15 +0900

    programmers 월간 코드 챌린지 시즌3 없는 숫자 더하기

---
+++ programmers/없는숫자더하기.java
@@ -0,0 +1,22 @@
+package programmers;
+
+public class 없는숫자더하기 {
+
+    final static int ZEROTONINE = 45;
+
+    public static int solution(int[] numbers) {
+        int answer = 0;
+
+        for (int number : numbers) {
+            answer += number;
+        }
+
+        return ZEROTONINE - answer;
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 6, 7, 8, 0};
+
+        System.out.println(solution(numbers));
+    }
+}

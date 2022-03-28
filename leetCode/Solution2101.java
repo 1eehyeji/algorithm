08bdbae3b9e07286afeb2f32566910d5979a607f
1eehyeji <leehj.kk@gmail.com>
2022-03-28 22:08:13 +0900

    improves runtime 133ms -> 51ms

--- leetCode/Solution2101.java
+++ leetCode/Solution2101.java
@@ -1,19 +1,15 @@
 package leetCode;
 
-import java.util.*;
-
 // Detonate the Maximum Bombs
 public class Solution2101 {
 
     public static int maximumDetonation(int[][] bombs) {
-        List<Integer>[] graph = new ArrayList[bombs.length];
+        boolean[][] graph = new boolean[bombs.length][bombs.length];
 
         for (int i = 0; i < bombs.length; ++i) {
             int x = bombs[i][0];
             int y = bombs[i][1];
             int r = bombs[i][2];
-
-            List<Integer> list = new ArrayList<>();
 
             for (int j = 0; j < bombs.length; ++j) {
                 if (i == j) continue;
@@ -22,26 +18,27 @@ public class Solution2101 {
                 int y2 = bombs[j][1];
 
                 if (Math.pow(r, 2) >= (Math.pow(x - x2, 2) + Math.pow(y - y2, 2))) {
-                    list.add(j);
+                    graph[i][j] = true;
                 }
             }
-
-            graph[i] = list;
         }
 
         int max = 0;
 
-        boolean[] isChecked;
-
         for (int i = 0; i < bombs.length; ++i) {
-            isChecked = new boolean[bombs.length];
-            max = Math.max(max, dfs(graph, isChecked, i));
+            int k = dfs(graph, new boolean[bombs.length], i);
+
+            if (k == bombs.length) {
+                return k;
+            }
+
+            max = Math.max(max, k);
         }
 
         return max;
     }
 
-    private static int dfs(List<Integer>[] graph, boolean[] isChecked, int n) {
+    private static int dfs(boolean[][] graph, boolean[] isChecked, int n) {
         if (isChecked[n]) {
             return 0;
         }
@@ -50,8 +47,10 @@ public class Solution2101 {
 
         int count = 1;
 
-        for (int i = 0; i < graph[n].size(); ++i) {
-            count += dfs(graph, isChecked, graph[n].get(i));
+        for (int i = 0; i < graph.length; ++i) {
+            if (graph[n][i]) {
+                count += dfs(graph, isChecked, i);
+            }
         }
 
         return count;

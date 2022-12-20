1c1418a2918549fecfea3c84216b69199b1161ff
1eehyeji <leehj.kk@gmail.com>
2022-12-20 23:06:30 +0900

    programmers level 2 2021 kakao blind 순위 검색 효율성 문제 해결

--- programmers/kakao2021/순위검색.java
+++ programmers/kakao2021/순위검색.java
@@ -1,91 +1,92 @@
 package programmers.kakao2021;
 
 import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class 순위검색 {
 
-    String[][] conditions = {{"cpp", "java", "python"},
-            {"backend", "frontend"},
-            {"junior", "senior"},
-            {"chicken", "pizza"}};
-
-    public int[] solution(String[] info, String[] query) {
+    public int[] solution(String[] infos, String[] query) {
         int[] answer = new int[query.length];
 
         Map<String, List<Integer>> infoMap = new HashMap<>();
 
-        Pattern conditionPattern = Pattern.compile("([a-z\\s]+)\\s([0-9]+)");
+        for (String info : infos) {
+            String[] infoTokens = info.split(" ");
 
-        for (String infoOfApplicant : info) {
-            Matcher matcher = conditionPattern.matcher(infoOfApplicant);
-
-            if (!matcher.find()) {
-                continue;
-            }
-
-            String condition = matcher.group(1);
-            String point = matcher.group(2);
-
-            infoMap.put(condition, infoMap.getOrDefault(condition, new ArrayList<>()));
-            infoMap.get(condition).add(Integer.parseInt(point));
+            setInfoMap(infoMap, infoTokens, 0, new String[infoTokens.length - 1]);
         }
 
-        Pattern queryPattern = Pattern.compile("([a-z\\s\\-]+)\\s([0-9]+)");
+        infoMap.keySet().forEach(key -> Collections.sort(infoMap.get(key)));
 
         for (int i = 0; i < query.length; ++i) {
-            Matcher matcher = queryPattern.matcher(query[i]);
+            String[] queryTokens = query[i].split(" and | ");
 
-            if (!matcher.find()) {
-                continue;
-            }
+            String requiredCondition = String.join(" ", Arrays.copyOf(queryTokens, 4));
 
-            String condition = matcher.group(1);
-            int point = Integer.parseInt(matcher.group(2));
+            int requiredScore = Integer.parseInt(queryTokens[queryTokens.length - 1]);
 
-            String[] token = condition.split(" and ");
-            List<String> conditionList = new ArrayList<>();
-            getConditionString(0, token.length, token, "", conditionList);
+            if (!infoMap.containsKey(requiredCondition)) continue;
 
-            for (String s : conditionList) {
-                if (infoMap.get(s) == null) {
-                    continue;
-                }
-
-                for (int p : infoMap.get(s)) {
-                    if (point <= p) {
-                        ++answer[i];
-                    }
-                }
-            }
+            List<Integer> scoreList = infoMap.get(requiredCondition);
+            answer[i] = scoreList.size() - lowerBound(scoreList, requiredScore);
         }
 
         return answer;
     }
 
-    private void getConditionString(int start, int end, String[] token, String condition, List<String> conditionList) {
-        if (start == end) {
-            conditionList.add(condition.trim());
-            return;
-        }
+    private int lowerBound(List<Integer> list, int requiredScore) {
+        int left = 0;
+        int right = list.size() - 1;
 
-        if (token[start].equals("-")) {
-            for (String s : conditions[start]) {
-                getConditionString(start + 1, end, token, condition + s + " ", conditionList);
+        int mid;
+
+        while (right >= left) {
+            mid = (left + right) / 2;
+
+            if (list.get(mid) < requiredScore) {
+                left = mid + 1;
+                continue;
             }
+
+            right = mid - 1;
+        }
+
+        return left;
+    }
+
+    private void setInfoMap(Map<String, List<Integer>> infoMap, String[] infoTokens, int index, String[] tokens) {
+        if (index == infoTokens.length - 1) {
+            String info = String.join(" ", tokens);
+            infoMap.put(info, infoMap.getOrDefault(info, new ArrayList<>()));
+            infoMap.get(info).add(Integer.parseInt(infoTokens[index]));
             return;
         }
 
-        getConditionString(start + 1, end, token, condition + token[start] + " ", conditionList);
+        tokens[index] = infoTokens[index];
+        setInfoMap(infoMap, infoTokens, index + 1, tokens);
+        tokens[index] = "-";
+        setInfoMap(infoMap, infoTokens, index + 1, tokens);
     }
 
     public static void main(String[] args) {
         순위검색 rankingSearcher = new 순위검색();
 
-        System.out.println(rankingSearcher.solution(
-                new String[]{"java backend junior pizza 150", "python frontend senior chicken 210", "python frontend senior chicken 150", "cpp backend senior pizza 260", "java backend junior chicken 80", "python backend senior chicken 50"},
-                new String[]{"java and backend and junior and pizza 100", "python and frontend and senior and chicken 200", "cpp and - and senior and pizza 250", "- and backend and senior and - 150", "- and - and - and chicken 100", "- and - and - and - 150"}
-        ));
+        System.out.println(Arrays.toString(rankingSearcher.solution(
+                new String[]{
+                        "java backend junior pizza 150",
+                        "python frontend senior chicken 210",
+                        "python frontend senior chicken 150",
+                        "cpp backend senior pizza 260",
+                        "java backend junior chicken 80",
+                        "python backend senior chicken 50"
+                },
+                new String[]{
+                        "java and backend and junior and pizza 100",
+                        "python and frontend and senior and chicken 200",
+                        "cpp and - and senior and pizza 250",
+                        "- and backend and senior and - 150",
+                        "- and - and - and chicken 100",
+                        "- and - and - and - 150"
+                }
+        )));
     }
 }

292ae9bc8c2bd1f86becb68ee00ff23173abc884
1eehyeji <leehj.kk@gmail.com>
2023-01-02 16:03:02 +0900

    programmers level 2 마법의 엘리베이터

---
+++ programmers/마법의엘리베이터.java
@@ -0,0 +1,23 @@
+package programmers;
+
+public class 마법의엘리베이터 {
+    public int solution(int storey) {
+        int answer = 0;
+
+        while(storey > 0) {
+            int n = storey % 10;
+            storey /= 10;
+
+            if (n < 5 || (n == 5 && (storey % 10) < 5)) {
+                answer += n;
+                continue;
+            }
+
+            answer += (10 - n);
+            storey += 1;
+
+        }
+
+        return answer;
+    }
+}
